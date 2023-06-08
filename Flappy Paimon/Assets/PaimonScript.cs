dcb49743aeb41e8cee1cc3d9ea546bea491e8c11
Arman Hossain Dipu Khan <80636334+ArmanDipu@users.noreply.github.com>
2023-06-08 16:45:26 +0600

    reject player control after game over

--- Flappy Paimon/Assets/PaimonScript.cs
+++ Flappy Paimon/Assets/PaimonScript.cs
@@ -7,6 +7,7 @@ public class PaimonScript : MonoBehaviour
     public Rigidbody2D paimonRigidBody2D;
     public float flapStrength = 7.5f;
     public LogicScript logicScript;
+    private bool isPaimonAlive = true;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +18,7 @@ public class PaimonScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if(Input.GetKeyDown(KeyCode.Space) && isPaimonAlive){
             paimonRigidBody2D.velocity = Vector2.up * flapStrength;
         }
     }
@@ -25,5 +26,6 @@ public class PaimonScript : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision){
         Debug.Log("Collision Happened");
         logicScript.gameOver();
+        isPaimonAlive = false;
     }
 }
