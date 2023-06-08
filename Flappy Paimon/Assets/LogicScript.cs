deba3a518686b6412fa35b1900a2a6b5e2160cab
Arman Hossain Dipu Khan <80636334+ArmanDipu@users.noreply.github.com>
2023-06-08 16:32:17 +0600

    Created Game Over Logic

--- Flappy Paimon/Assets/LogicScript.cs
+++ Flappy Paimon/Assets/LogicScript.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
+    public GameObject gameOverScreen;
 
-    [ContextMenu("Add Score")]
     public void addScore(int score){
         playerScore+= score;
         scoreText.text = playerScore.ToString();
@@ -18,5 +19,14 @@ public class LogicScript : MonoBehaviour
     public void resetScore(){
         playerScore = 0;
         scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    [ContextMenu("Call Game Over")]
+    public void gameOver(){
+        gameOverScreen.SetActive(true);
     }
 }
--- Flappy Paimon/Assets/PaimonScript.cs
+++ Flappy Paimon/Assets/PaimonScript.cs
@@ -6,11 +6,12 @@ public class PaimonScript : MonoBehaviour
 {
     public Rigidbody2D paimonRigidBody2D;
     public float flapStrength = 7.5f;
+    public LogicScript logicScript;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -19,5 +20,10 @@ public class PaimonScript : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space)){
             paimonRigidBody2D.velocity = Vector2.up * flapStrength;
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        Debug.Log("Collision Happened");
+        logicScript.gameOver();
     }
 }
